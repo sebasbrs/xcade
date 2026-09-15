7acb8ea0a7a6d0fef529a5a5c527469ac099229c
sebasbrs <sebasgodoy32@gmail.com>
2026-09-15 11:12:17 -0500

    Actualizar URLs y agregar configuración de CNAME para el sitio web

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://xcade.org',
+  site: 'https://www.xcade.org',
   output: 'static',
   integrations: [sitemap()],
   vite: {
