7e087c3ff66b2fb6ae86b0479125504cfc2b43c5
Sebastian Godoy <sebasgodoy32@gmail.com>
2026-09-15 12:58:45 -0500

    Reduce default game limit from 10000 to 1500

--- src/lib/games.ts
+++ src/lib/games.ts
@@ -1,5 +1,5 @@
 const GAME_FEED_URL = 'https://gamemonetize.com/feed.php?format=0';
-const DEFAULT_GAME_LIMIT = 10000;
+const DEFAULT_GAME_LIMIT = 1500;
 
 export interface Game {
     title: string;
